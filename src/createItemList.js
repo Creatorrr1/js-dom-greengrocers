ef0bf7a5dbc48d738e1077a9e6e35efd801223b2
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-04-25 15:50:45 +0100

    update 2

--- src/createItemList.js
+++ src/createItemList.js
@@ -12,27 +12,100 @@
 const storeItemListUl = document.querySelector('.store--item-list')
 const cartItemListUl =document.querySelector('.cart--item-list')
 
-state.items.forEach(item => {
-//   const product = state.items[i];
-  const currentItem = item
-  console.log(currentItem);
+function renderStore() {
 
-  //create and add an li to ul
-  const itemLi = document.createElement("li");
-  storeItemListUl.append(itemLi);
+    state.items.forEach(item => {
+        //   const product = state.items[i];
+        const currentItem = item
+        console.log(currentItem);
+        
+        //create and add an li to ul
+        const itemLi = document.createElement("li");
+        storeItemListUl.append(itemLi);
+        
+        //create and add div that should hold the img to li
+        const divForImg = document.createElement("div");
+        divForImg.setAttribute("class", "store--item-icon");
+        itemLi.append(divForImg);
+        
+        //create the img and add to the div
+        const itemImg = document.createElement("img");
+        itemImg.src = `assets/icons/${currentItem.id}.svg`;
+        divForImg.append(itemImg);
+        
+        //create and add img to li
+        const addCartBtn = document.createElement("button");
+        addCartBtn.innerHTML = "Add to cart";
+        itemLi.append(addCartBtn);
+          addCartBtnEvent(addCartBtn, item)
+    });
+}
+    
+    // pushes item to cart, now i just need to get visibility
+function addCartBtnEvent (addCartBtn, currentItem) {
+    addCartBtn.addEventListener('click', () => {
+        state.cart.push(currentItem)
+        renderCart()
+        console.log(state.cart)
+        // return state.cart
+    })
+}
 
-  //create and add div to ul
-  const divForImg = document.createElement("div");
-  divForImg.setAttribute("class", "store--item-icon");
-  itemLi.append(divForImg);
+function renderCart() {
+    cartItemListUl.innerHTML = ""
 
-  //create and add img to ul
-  const itemImg = document.createElement("img");
-  itemImg.src = `assets/icons/${currentItem.id}.svg`;
-  divForImg.append(itemImg);
+    state.cart.forEach(item => {
+        // if (itemAdded === items) {
+            const cartItem = item
+            console.log(cartItem)
+        // }
+        const itemName = cartItem.name
+        console.log()
+        const itemID = cartItem.id
+        
+        // add li to ul
+        const cartItemLi = document.createElement("li")
+        
+        // add img to cart
+        const cartImg = document.createElement('img')
+        cartImg.className = 'cart--item-icon'
+        cartImg.src = `assets/icons/${itemID}.svg`
+        cartImg.alt = itemName
+        cartItemLi.append(cartImg)
+        
+        // add description to cart
+        const nameInPTag = document.createElement("p")
+        nameInPTag.innerText = itemName
+        cartItemLi.append(nameInPTag)
+        
+        // remove item when remove button is clicked
+        const removeBtn = document.createElement('button')
+        // removeBtn.className = "quantity-btn remove-btn center"
+        removeBtn.className = "quantity-btn remove-btn center"
+        removeBtn.innerText = "-"
+        cartItemLi.append(removeBtn)
+        removeBtn.addEventListener('click', () => {
+            cartItemLi.remove()
+        })
+        
+        // quantity feature needs to be added
+        
 
-  //create and add img to ul
-  const addCartBtn = document.createElement("button");
-  addCartBtn.innerHTML = "Add to cart";
-  itemLi.append(addCartBtn);
-});
+        // add item when add button is clicked
+        const addBtn = document.createElement('button')
+        // addBtn.className = "quantity-btn remove-btn center"
+        addBtn.className = "quantity-btn add-btn center"
+        
+        addBtn.innerText = "+"
+        cartItemLi.append(addBtn)
+        removeBtn.addEventListener('click', () => {
+            
+        })
+        
+        cartItemListUl.append(cartItemLi)
+        // return cartItemLi
+        
+    })
+}
+    
+renderStore()
